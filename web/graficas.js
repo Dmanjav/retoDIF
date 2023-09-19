3313d0d43d2d08f701d0ebc05e93bf9251b55b6d
Diego Manja <94866755+Dmanjav@users.noreply.github.com>
2023-09-19 16:52:19 -0600

    implementación chartjs

---
+++ web/graficas.js
@@ -0,0 +1,80 @@
+// Crea un objeto gráfico
+var myChart = new Chart(document.getElementById("myChart"), {
+    // Define el tipo de gráfico
+    type: "bar",
+    // Define los datos
+    data: {
+        labels: ["Enero", "Febrero", "Marzo", "Abril", "Mayo"],
+        datasets: [{
+            label: "Ventas",
+            data: [100, 200, 300, 400, 500]
+        }]
+    }
+});
+
+var myChart = new Chart(document.getElementById("myChart2"), {
+    // Define el tipo de gráfico
+    type: "pie",
+    // Define los datos
+    data: {
+        labels: ["Enero", "Febrero", "Marzo", "Abril", "Mayo"],
+        datasets: [{
+            label: "Ventas",
+            data: [100, 200, 300, 400, 500]
+        }]
+    }
+});
+
+// Crea un objeto gráfico
+var myChart = new Chart(document.getElementById("myChart3"), {
+    // Define el tipo de gráfico
+    type: "bar",
+    // Define los datos
+    data: {
+        labels: ["Enero", "Febrero", "Marzo", "Abril", "Mayo"],
+        datasets: [{
+            label: "Ventas",
+            data: [100, 200, 300, 400, 500]
+        }]
+    }
+});
+
+var myChart = new Chart(document.getElementById("myChart4"), {
+    // Define el tipo de gráfico
+    type: "pie",
+    // Define los datos
+    data: {
+        labels: ["Enero", "Febrero", "Marzo", "Abril", "Mayo"],
+        datasets: [{
+            label: "Ventas",
+            data: [100, 200, 300, 400, 500]
+        }]
+    }
+});
+
+// Crea un objeto gráfico
+var myChart = new Chart(document.getElementById("myChart5"), {
+    // Define el tipo de gráfico
+    type: "bar",
+    // Define los datos
+    data: {
+        labels: ["Enero", "Febrero", "Marzo", "Abril", "Mayo"],
+        datasets: [{
+            label: "Ventas",
+            data: [100, 200, 300, 400, 500]
+        }]
+    }
+});
+
+var myChart = new Chart(document.getElementById("myChart6"), {
+    // Define el tipo de gráfico
+    type: "pie",
+    // Define los datos
+    data: {
+        labels: ["Enero", "Febrero", "Marzo", "Abril", "Mayo"],
+        datasets: [{
+            label: "Ventas",
+            data: [100, 200, 300, 400, 500]
+        }]
+    }
+});
