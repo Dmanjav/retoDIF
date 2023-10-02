b9829574ddbf7d2f8e2df9435f1ff4d8d7e7b66f
Aldehil Sánchez <sanheraldehil@outlook.com>
2023-10-02 12:12:16 -0600

    Queries para el dashboard

--- web/connection.py
+++ web/connection.py
@@ -34,3 +34,35 @@ class connection():
             DESC LIMIT 10;'''
         self.cursor.execute(query)
         return self.cursor.fetchall()
+
+    def get_ventasDia_comedor(self, idComedor):
+        '''
+        Returns number of sales of one kitchen by day of the week
+        '''
+        query = '''SELECT DAYOFWEEK(fechaHora) AS dia_de_la_semana,
+            COUNT(*) AS ventas 
+            FROM Pedido WHERE idComedor = %s 
+            GROUP BY DAYOFWEEK(fechaHora) 
+            ORDER BY dia_de_la_semana;'''
+        self.cursor.execute(query, [idComedor])
+        return self.cursor.fetchall()
+
+    def get_ventasHora_comedor(self, idComedor):
+        '''
+        Returns number of sales of one kitchen by hour of the day
+        '''
+        query = '''SELECT HOUR(fechaHora) AS hora_del_dia, 
+            COUNT(*) AS ventas FROM Pedido 
+            WHERE idComedor = %s GROUP BY HOUR(fechaHora)
+            ORDER BY hora_del_dia;'''
+        self.cursor.execute(query, [idComedor])
+        return self.cursor.fetchall()
+
+    def get_num_dependencia(self):
+        '''Returns the number of people that depends or not on other'''
+        query = '''SELECT ( CASE WHEN idResponsable = idDependiente 
+            THEN 'Independiente' ELSE 'Dependiente' END ) 
+            AS tipo_de_usuario, COUNT(*) AS cantidad FROM Dependencia 
+            GROUP BY tipo_de_usuario ORDER BY tipo_de_usuario;'''
+        self.cursor.execute(query)
+        return self.cursor.fetchall()
