ace3dc7d757412b08d9f071fa3b4214701c14904
Diego Manja <94866755+Dmanjav@users.noreply.github.com>
2023-10-04 18:55:15 -0600

    Todas las sucursales

--- web/password.py
+++ web/password.py
@@ -1,6 +1,7 @@
 from werkzeug.security import generate_password_hash, check_password_hash
-password = 'CENTRAL'
+password = 'Amp. Adolfo Lopez Mateos'
 hashed_pass = generate_password_hash(password)
 
 print(hashed_pass)
 print(check_password_hash(hashed_pass,password))
+print(password)
