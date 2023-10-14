1b217930c4ccb08ecbb1d5276e623e8bb73d4c85
A01754650 <88682624+A01754650@users.noreply.github.com>
2023-10-14 00:10:08 -0600

    Cambio de diseño en Pantalla principal y Log in

--- DifoodAme/app/src/main/java/mx/itesm/difoodame/view/LoginView.kt
+++ DifoodAme/app/src/main/java/mx/itesm/difoodame/view/LoginView.kt
@@ -25,8 +25,12 @@ class LoginView : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_view)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+
+
         registrarEventos()
+        registrarObservables()
+
+
     }
 
     private fun registrarObservables() {
